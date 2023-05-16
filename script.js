9eb0b708815338bcb2d977a351fdd80519eebc83
Ben Tolang <bentol.webdev@gmail.com>
2023-05-17 06:06:38 +0800

    update script to show navbar bugger when scrolled

--- script.js
+++ script.js
@@ -9,11 +9,13 @@ if (window.scrollY >= 10) {
 window.onscroll = () => {
   if (this.scrollY <= 10) {
     nav.classList.remove('scrolled')
+    nav.classList.remove('theme-light')
     nav.classList.remove('navbar-light')
     nav.classList.add('navbar-dark')
     nav.classList.add('bg-dark')
   } else {
     nav.classList.add('scrolled')
+    nav.classList.add('theme-light')
     nav.classList.add('navbar-light')
     nav.classList.remove('navbar-dark')
     nav.classList.remove('bg-dark')
