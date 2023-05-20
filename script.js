9433fa9889a908d0fbc71e8454f888b23d7b4d31
Ben Tolang <bentol.webdev@gmail.com>
2023-05-20 20:08:13 +0800

    remove bg-dark from navbar, edit js script to remove bg-dark being added back when page scrolls up, add pl-3 to navbar-brand on xs screen sizes only

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ window.onscroll = () => {
     nav.classList.remove('theme-light')
     nav.classList.remove('navbar-light')
     nav.classList.add('navbar-dark')
-    nav.classList.add('bg-dark')
   } else {
     nav.classList.add('scrolled')
     nav.classList.add('theme-light')
     nav.classList.add('navbar-light')
     nav.classList.remove('navbar-dark')
-    nav.classList.remove('bg-dark')
   }
 };
 
